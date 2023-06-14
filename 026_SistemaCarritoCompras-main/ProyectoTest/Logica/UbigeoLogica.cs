9b56ac9a140122861cc823d769692f7ab9340be6
stanleeeys <69662538+stanleeeys@users.noreply.github.com>
2023-06-14 10:41:33 -0600

    Initial commit

---
+++ 026_SistemaCarritoCompras-main/ProyectoTest/Logica/UbigeoLogica.cs
@@ -0,0 +1,132 @@
+﻿using ProyectoTest.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace ProyectoTest.Logica
+{
+    public class UbigeoLogica
+    {
+        
+        private static UbigeoLogica _instancia = null;
+
+        public UbigeoLogica()
+        {
+
+        }
+
+        public static UbigeoLogica Instancia
+        {
+            get {
+                if (_instancia == null) {
+                    _instancia = new UbigeoLogica();
+                }
+                return _instancia;
+            }
+        }
+
+
+
+        public List<Departamento> ObtenerDepartamento() {
+            List<Departamento> lst = new List<Departamento>();
+            using (SqlConnection oConexion = new SqlConnection(Conexion.CN))
+            {
+                try
+                {
+                    SqlCommand cmd = new SqlCommand("select * from departamento", oConexion);
+                    cmd.CommandType = CommandType.Text;
+                    oConexion.Open();
+                    using (SqlDataReader dr = cmd.ExecuteReader())
+                    {
+                        while (dr.Read())
+                        {
+                            lst.Add(new Departamento()
+                            {
+                                IdDepartamento = dr["IdDepartamento"].ToString(),
+                                Descripcion = dr["Descripcion"].ToString()
+                            });
+                        }
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    lst = new List<Departamento>();
+                }
+            }
+            return lst;
+        }
+
+        public List<Municipio> ObtenerMunicipio(string _iddepartamento)
+        {
+            List<Municipio> lst = new List<Municipio>();
+            using (SqlConnection oConexion = new SqlConnection(Conexion.CN))
+            {
+                try
+                {
+                    SqlCommand cmd = new SqlCommand("select * from municipio where IdDepartamento = @iddepartamento", oConexion);
+                    cmd.Parameters.AddWithValue("@iddepartamento", _iddepartamento);
+                    cmd.CommandType = CommandType.Text;
+                    oConexion.Open();
+                    using (SqlDataReader dr = cmd.ExecuteReader())
+                    {
+                        while (dr.Read())
+                        {
+                            lst.Add(new Municipio()
+                            {
+                                IdMunicipio = dr["IdMunicipio"].ToString(),
+                                Descripcion = dr["Descripcion"].ToString(),
+                                IdDepartamento = dr["IdDepartamento"].ToString()
+                            });
+                        }
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    lst = new List<Municipio>();
+                }
+            }
+            return lst;
+        }
+
+        public List<Distrito> ObtenerDistrito(string _idmunicipio, string _iddepartamento)
+        {
+            List<Distrito> lst = new List<Distrito>();
+            using (SqlConnection oConexion = new SqlConnection(Conexion.CN))
+            {
+                try
+                {
+                    SqlCommand cmd = new SqlCommand("select * from DISTRITO where IdMunicipio = @idmunicipio and IdDepartamento = @iddepartamento", oConexion);
+                    cmd.Parameters.AddWithValue("@idmunicipio", _idmunicipio);
+                    cmd.Parameters.AddWithValue("@iddepartamento", _iddepartamento);
+                    cmd.CommandType = CommandType.Text;
+                    oConexion.Open();
+                    using (SqlDataReader dr = cmd.ExecuteReader())
+                    {
+                        while (dr.Read())
+                        {
+                            lst.Add(new Distrito()
+                            {
+                                IdDistrito = dr["IdDistrito"].ToString(),
+                                Descripcion = dr["Descripcion"].ToString(),
+                                IdMunicipio = dr["IdMunicipio"].ToString(),
+                                IdDepartamento = dr["IdDepartamento"].ToString()
+                            });
+                        }
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    lst = new List<Distrito>();
+                }
+            }
+            return lst;
+        }
+
+    }
+}
